d15b12280ad31ed7bd2be1d99c62eaae3a27fc13
Arham <arhamn205@gmail.com>
2022-12-18 00:20:16 +0500

    removed useless code

--- src/utils/Navbar.tsx
+++ src/utils/Navbar.tsx
@@ -1,12 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function Navbar() {
-    return (
-      <>
-        <div className="flex justify-evenly h-40 mt-2 text-6xl relative left-10 ">
-          <div>Question:</div>
-          <div>Score: </div>
-        </div>
-      </>
-    );
-}
+export default function Navbar(props: any) {
+
+   
+    
+  }
