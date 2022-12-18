37c9fd6f91afe3755ac8509dd166613701d21f66
Arham <arhamn205@gmail.com>
2022-12-18 20:51:55 +0500

    separated into different components

--- src/Questions/Question.tsx
+++ src/Questions/Question.tsx
@@ -1,23 +1,92 @@
-import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
+import React, {
+  Suspense,
+  Dispatch,
+  SetStateAction,
+  useEffect,
+  useState,
+} from "react";
 import { useQuery } from "@tanstack/react-query";
 import Axios from "axios";
+import Result from "../pages/Result";
 import { useLocation } from "react-router-dom";
-import Structure from "./Structure";
+// import Structure from "./Structure";
+const Structure = React.lazy(() => import("./Structure"));
 
 export default function Question() {
- const [qNumber, setQNumber] = useState(-1);
+  const [qNumber, setQNumber] = useState<number>(-1);
+  const [score , setScore] = useState<number>(0);
 
-  const { data:easyQuestion, refetch:easyRefetch, isLoading:loadEasy, error:errorEasy } = useQuery(["question"], async () => {
+  const {
+    data: easyQuestion,
+    refetch: easyRefetch,
+    isLoading: loadEasy,
+    error: errorEasy,
+  } = useQuery(["question"], async () => {
     return await Axios.get(
-      "https://the-trivia-api.com/api/questions?categories=arts_and_literature&limit=1&difficulty=easy"
+      "https://the-trivia-api.com/api/questions?categories=general_knowledge,science,geography,history&limit=1&difficulty=easy"
     ).then((res) => res.data);
   });
- 
-  return (
-    <>
 
-          <Structure data={easyQuestion} refetch={easyRefetch} isLoading={loadEasy} error={errorEasy} qNumber={qNumber} setQNumber={setQNumber} />
-  
-    </>
+  const {
+    data: medQuestion,
+    refetch: medRefetch,
+    isLoading: loadMed,
+    error: errorMed,
+  } = useQuery(["question"], async () => {
+    return await Axios.get(
+      "https://the-trivia-api.com/api/questions?categories=general_knowledge,science,geography,history&limit=1&difficulty=medium"
+    ).then((res) => res.data);
+  });
+
+  const {
+    data: hardQuestion,
+    refetch: hardRefetch,
+    isLoading: loadHard,
+    error: errorHard,
+  } = useQuery(["question"], async () => {
+    return await Axios.get(
+      "https://the-trivia-api.com/api/questions?categories=general_knowledge,science,geography,history&limit=1&difficulty=hard"
+    ).then((res) => res.data);
+  });
+
+  return (
+    <Suspense fallback={<div>Loading..</div>}>
+      {qNumber <= 7 ? (
+        <Structure
+          data={easyQuestion}
+          refetch={easyRefetch}
+          isLoading={loadEasy}
+          error={errorEasy}
+          qNumber={qNumber}
+          setQNumber={setQNumber}
+          score={score}
+          setScore={setScore}
+        />
+      ) : qNumber > 7 && qNumber <= 12 ? (
+        <Structure
+          data={medQuestion}
+          refetch={medRefetch}
+          isLoading={loadMed}
+          error={errorMed}
+          qNumber={qNumber}
+          setQNumber={setQNumber}
+          score={score}
+          setScore={setScore}
+        />
+      ) : qNumber > 12 && qNumber <= 20 ? (
+        <Structure
+          data={hardQuestion}
+          refetch={hardRefetch}
+          isLoading={loadHard}
+          error={errorHard}
+          qNumber={qNumber}
+          setQNumber={setQNumber}
+          score={score}
+          setScore={setScore}
+        />
+      ) : (
+              <Result score={score} />
+      )}
+    </Suspense>
   );
 }
